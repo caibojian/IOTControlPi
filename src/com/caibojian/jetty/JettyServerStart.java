57a4cab24022d0d1bedc0c3866a357bd3fa1f02e
Administrator <Administrator@WIN-cbj>
2016-06-28 17:57:38 +0800

    mavan版本转换，使用jetty作为服务器来运行

---
+++ src/com/caibojian/jetty/JettyServerStart.java
@@ -0,0 +1,10 @@
+package com.caibojian.jetty;
+//启动类，入口
+public class JettyServerStart {
+    public static void main(String[] args) {
+        JettyCustomServer server = new JettyCustomServer(
+                "./jetty/etc/jetty.xml", "/testContext");
+        server.startServer();
+
+    }
+}
